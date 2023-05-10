dfd9f4bb1347b182513f6c71fbd3fcd16153a73a
Noname-creat0r <egorchik228322@gmail.com>
2023-05-10 11:37:06 +0300

    feat(db): creating User model + migration

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { syncModels } = require("./db/database");
 
 require("dotenv").config({ path: "./config.env" });
 
 const app = express();
 const port = process.env.SERVER_PORT;
 
-const authRoutes = require('./auth/authRouter');
+const authRoutes = require("./auth/authRouter");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -22,12 +23,18 @@ app.use((req, res, next) => {
 app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
-  const status = erorr.statusCode || 500;
+  const status = error.statusCode || 500;
   const message = error.message || 'Internal server error';
   const data = error.data;
+  
   res.status(status).json({ message, data});
 });
 
-app.listen(port, () => {
-  console.log('Server is running on port: ' + port);
-})
+syncModels()
+  .then(() => {
+    app.listen(port, async () => {
+      console.log("Syncing models...");
+      console.log('Server is running on port: ' + port);
+    })
+  })
+
