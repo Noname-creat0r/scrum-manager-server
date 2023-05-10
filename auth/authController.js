dcdbc2795ff1bbc2ce2d015e3a99723cf8a90a55
Noname-creat0r <egorchik228322@gmail.com>
2023-05-10 16:41:19 +0300

    feat(components): creating Auth route + controllers (signup)

--- auth/authController.js
+++ auth/authController.js
@@ -1,15 +1,42 @@
 const jwt = require('jsonwebtoken');
-const { db } = require('../db/database');
+const db = require('../models');
 
 exports.signin = (req, res, next) => {
-  const { email, password } = req.body;
+  try { 
+    const { email, password } = req.body;
+     
 
-  
+  } catch(error) {
+    console.log(error);
+  }
 };
 
-exports.signup = (req, res, next) => {
-  const { username, email, password } = req.body;
-
+exports.signup = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
   
+    const isSignedUp = await db.User.findOne({
+     where: { name: name, email: email },
+    });
+  
+    if (isSignedUp) {
+      throw new Error("This user has already been signed up");
+    }
+  
+     const newUser = await db.User.create({
+       name: name,
+       email: email,
+       password: password
+    });
+  
+    res.status(201).json({
+      message: "New user has been signed up successfully",
+      userId: newUser.id
+    })
+  
+  } catch(error) {
+    console.log(error);
+    next(error);
+  }
 };
 
