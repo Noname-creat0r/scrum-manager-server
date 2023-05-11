4f48c7a22693ab81305fa73d540f4cf1c7e1cf8a
Noname-creat0r <egorchik228322@gmail.com>
2023-05-11 10:30:01 +0300

    feat(auth): signin + signup + isAuth middleware

--- auth/authController.js
+++ auth/authController.js
@@ -1,13 +1,39 @@
 const jwt = require('jsonwebtoken');
-const db = require('../models');
+const bcrypt = require('bcryptjs');
 
-exports.signin = (req, res, next) => {
+const db = require('../db/models');
+const { throwError } = require ('../util/error');
+
+exports.signin = async (req, res, next) => {
   try { 
     const { email, password } = req.body;
-     
+  
+    const user = await db.User.findOne({
+      where: { email: email }
+    })
+    if (!user) {
+      throwError(401, 'This user need to sign up first!')
+    }
+
+    const isRightPass = await bcrypt.compare(password, user.password);
+    if (!isRightPass) {
+      throwError(401, 'Wrong password!')
+    }
+
+    const key = process.env.KEY
+    const token = jwt.sign(
+      {
+        exp: Math.floor(Date.now() / 1000) + (60 * 60),
+        email: email,
+        userId: user.id.toString()
+      }, 
+      key
+    )
+
+    res.status(201).json({ id: user.id, token })
 
   } catch(error) {
-    console.log(error);
+    next(error);  
   }
 };
 
@@ -20,7 +46,7 @@ exports.signup = async (req, res, next) => {
     });
   
     if (isSignedUp) {
-      throw new Error("This user has already been signed up");
+      throwError(403, 'This user has already been signed up!')
     }
   
      const newUser = await db.User.create({
@@ -35,7 +61,6 @@ exports.signup = async (req, res, next) => {
     })
   
   } catch(error) {
-    console.log(error);
     next(error);
   }
 };
---
+++ auth/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+const { throwError } = require('../util/error')
+const key = process.env.KEY
+
+exports.isAuth = (req, res, next) => {
+  try {
+    const authHeader = req.get('Authorization')
+    if (!authHeader) {
+      throwError(401, 'Not authenticated.')
+    }
+
+    const token = authHeader
+    const decodedToken = jwt.verify(token, key)
+
+    if (!decodedToken) {
+      throwError(401, 'Bad token.')
+    }
+
+    req.locals.userId = decodedToken.payload.userId
+    next()
+  } catch (error) {
+    next(error)
+  }
+} 
