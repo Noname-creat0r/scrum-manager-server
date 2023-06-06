3c867a7cd8413f24a4cef1d4f787c7290c3a2275
Noname-creat0r <egorchik228322@gmail.com>
2023-06-07 01:44:43 +0300

    feat(components): project and task controllers got a CUD handlers

--- components/project/projectController.js
+++ components/project/projectController.js
@@ -42,13 +42,61 @@ exports.getProject = async (req, res, next) => {
 }
 
 exports.postProject = async (req, res, next) => {
+  try {
+    const title = req.body.title
+    const description = req.body.description
+    const private = req.body.private
+  
+    if (!title || !description || !private.toString()) {
+      throwError(400, 'Missing project data.')
+    }
 
+    const newProject = await db.Project.create({
+      title: title,
+      description: description,
+      private: private,
+      authorId: req.userId
+    })
+
+    if (!newProject) {
+      throwError(500, 'Could not create a new project. Server error.')
+    }
+
+    res.status(201).json({ message: 'You have create a project!', project: newProject })
+
+  } catch(error) {
+    next(error)
+  }
 }
 
 exports.deleteProject = async (req, res, next) => {
+  try {
+    const projectId = req.body.projectId
+    if (!projectId) {
+      throwError(400, 'Missing project id.')
+    }
+  
+    const project = await db.Project.findOne({
+      where: { id: projectId }
+    })
 
+    if (!project) {
+      throwError(400, 'There is no such project you trying to delete.')
+    }
+
+    await project.destroy()
+
+    res.status(200).json({ message: 'You have deleted a project!', projectId })
+
+  } catch(error) {
+
+  }
 }
 
 exports.putProject = async (req, res, next) => {
+  try {
+    
+  } catch(error) {
 
+  }
 }
--- components/task/taskController.js
+++ components/task/taskController.js
@@ -1,5 +1,19 @@
 const db = require('../../db/models');
 const { throwError } = require ('../../util/error');
+  
+const selectTaskById = async (id) => {
+  return await db.Task.findOne({
+    where: { id: id },
+    attributes: ['title', 'description', 'storyPoints', 'iterationId'],
+    include: [
+      {
+        model: db.TaskStatus,
+        as: 'status',
+        attributes: ['status']
+      }
+    ]
+  })
+}
 
 exports.getTasks = async (req, res, next) => {
   try {
@@ -9,20 +23,19 @@ exports.getTasks = async (req, res, next) => {
       throwError(400, 'Missing body parametr.')
     }
 
-    const iterationTasks = await db.Iteration.findAll({
+    const projectTasks = await db.Task.findAll({
       where: { projectId: projectId },
-      attributes: [], 
-      include: [ 
-        { model: db.Task, as: 'tasks', attributes: ['title', 'description', 'storyPoints', 'iterationId'] }
-      ] 
+      attributes: ['id', 'title', 'description', 'storyPoints', 'iterationId'],
+      include: [
+        {
+          model: db.TaskStatus,
+          as: 'status',
+          attributes: ['status']
+        }
+      ]
     })
-  
-    const tasks = []
-    for (const iteration of iterationTasks) {
-      tasks.push(...iteration.tasks)
-    }
-
-    res.status(200).json({ tasks })
+ 
+    res.status(200).json({ tasks: projectTasks })
 
   } catch(error) {
     next(error)
@@ -33,14 +46,126 @@ exports.getTask = () => {
 
 }
 
-exports.postTask = () => {
+exports.postTask = async (req, res, next) => {
+  try {
+    const title = req.body.title
+    const description = req.body.description
+    const storyPoints = req.body.storyPoints
+    const iterationId = req.body.iterationId | null
+    const projectId = req.body.projectId
+    const status = req.body.status
 
+    if (!title || !description || !storyPoints || !projectId || !status) {
+      throwError(400, 'Missing body param.')    
+    }
+      
+    const statusId = await db.TaskStatus.findOne({
+      where: { status: status },
+      attributes: ['id']
+    })  
+    
+    // Outsource the checks to util
+
+    if (!statusId) {
+      throwError(400, 'There is no such status for the task')
+    }
+
+    const project = await db.Project.findOne({
+      where: { id: projectId }
+    })
+
+    if (!project) {
+      throwError(400, 'There is no such project for the new task')
+    }
+  
+    const newTask = await db.Task.create({
+      title: title,
+      description: description,
+      storyPoints: storyPoints,
+      projectId: projectId,
+      iterationId: null,
+      statusId: statusId.id
+    })
+
+    const task = await selectTaskById(newTask.id) 
+
+    res.status(201).json({ task })
+
+  } catch (error) {
+    next(error)
+  } 
 }
 
-exports.deleteTask = () => {
+exports.deleteTask = async (req, res, next) => {
+  try {
+    const taskId =  req.query.taskId
 
+    if (!taskId) {
+      throwError(400, 'Missing param in query.')
+    }
+
+    const task = await db.Task.findOne({
+      where: { id: taskId }
+    }) 
+
+    if (!task) {
+      throwError(400, 'There is no such task to delete.')
+    }
+
+    await task.destroy()
+
+    res.status(200).json({ message: 'You have deleted a task.', taskId})
+
+  } catch (error) {
+    next(error)
+  } 
 }
 
-exports.putTask = () => {
+exports.putTask = async (req, res, next) => {
+  try {
+    const taskId = req.body.taskId
+    const title = req.body.title
+    const description = req.body.description
+    const storyPoints = req.body.storyPoints
+    const iterationId = req.body.iterationId | null
+    const projectId = req.body.projectId
+    const status = req.body.status
 
+    if (!taskId) {
+      throwError(400, 'Missing param in body.')
+    }
+
+    const task = await db.Task.findOne({
+      where: { id: taskId }
+    })
+
+    if (!task) {
+      throwError(400, 'There is no such task to edit.')
+    }
+    
+    const statusId = await db.TaskStatus.findOne({
+      where: { status: status },
+      attributes: ['id']
+    })  
+
+    if (!statusId) {
+      throwError(400, 'There is no such status for the task')
+    }
+
+    task.set({
+      title: title || task.title,
+      description: description || task.description,
+      storyPoints: storyPoints || task.storyPoints,
+      iterationId: iterationId || task.iterationId,
+      projectId: projectId || task.projecId,
+      statusId: statusId.id || task.statusId
+    })
+
+    const updatedTask = await selectTaskById(task.id)
+
+    res.status(200).json({ message: 'You have updated a task information.', task: updatedTask })
+
+  } catch (error) {
+    next(error)
+  } 
 }
